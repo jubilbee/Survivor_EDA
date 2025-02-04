e58cf480ce0a4dc4604aa4f474d7ddf233933439
Camilla Babb <camilla.babb@gmail.com>
2025-02-03 20:44:51 -0500

    Updated Season column dtype in contestant table

--- scraper.py
+++ scraper.py
@@ -7,29 +7,26 @@ import pandas as pd
 #season overview table
 URL = "https://en.wikipedia.org/wiki/Survivor_(American_TV_series)"
 page = requests.get(URL)
-
 soup  = BeautifulSoup(page.content, 'html.parser')
-
 
 tables = soup.find_all('table', class_='wikitable')
 table = tables[0]
-season_table = pd.read_html(StringIO(str(table)))
 
+season_table = pd.read_html(StringIO(str(table)))
 season_table = pd.concat(season_table, axis=0).reset_index(drop=True)
 
 # Should I turn these into functions? And call them in j notebook, or should i write to csv at end
 #contestant table
 URL = "https://en.wikipedia.org/wiki/List_of_Survivor_(American_TV_series)_contestants"
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
 
-
 tables = soup.find_all('table', class_='wikitable')
-contestant_table = pd.read_html(StringIO(str(tables)))
 
+contestant_table = pd.read_html(StringIO(str(tables)))
 contestant_table = pd.concat(contestant_table, axis=0).reset_index(drop=True)
 
+season_table.loc[season_table['Subtitle'] == 'Borneo[c]', 'Subtitle'] = 'Borneo'
 
 def get_gender(main, gender):
     gender_dict = {}
@@ -134,13 +131,55 @@ def get_lgbt(main):
 lgbt_dict = get_lgbt('https://survivor.fandom.com/wiki/Category:LGBT_Contestants')
 
 
+contestant_table.replace({'Survivor: Borneo': 1, 
+    'Survivor: The Australian Outback': 2,
+    'Survivor: Africa': 3, 
+    'Survivor: Marquesas': 4, 
+    'Survivor: Thailand': 5, 
+    'Survivor: The Amazon': 6, 
+    'Survivor: Pearl Islands': 7,
+    'Survivor: All-Stars': 8, 
+    'Survivor: Vanuatu': 9, 
+    'Survivor: Palau': 10, 
+    'Survivor: Guatemala': 11,
+    'Survivor: Panama': 12, 
+    'Survivor: Cook Islands': 13, 
+    'Survivor: Fiji': 14, 
+    'Survivor: China': 15, 
+    'Survivor: Micronesia': 16, 
+    'Survivor: Gabon': 17, 
+    'Survivor: Tocantins': 18, 
+    'Survivor: Samoa': 19, 
+    'Survivor: Heroes vs. Villains': 20, 
+    'Survivor: Nicaragua': 21, 
+    'Survivor: Redemption Island': 22, 
+    'Survivor: South Pacific': 23, 
+    'Survivor: One World': 24, 
+    'Survivor: Philippines': 25, 
+    'Survivor: Caramoan': 26, 
+    'Survivor: Blood vs. Water': 27, 
+    'Survivor: Cagayan': 28, 
+    'Survivor: San Juan del Sur': 29, 
+    'Survivor: Worlds Apart': 30, 
+    'Survivor: Cambodia': 31, 
+    'Survivor: Kaôh Rōng': 32, 
+    'Survivor: Millennials vs. Gen X': 33, 
+    'Survivor: Game Changers': 34, 
+    'Survivor: Heroes vs. Healers vs. Hustlers': 35, 
+    'Survivor: Ghost Island': 36, 
+    'Survivor: David vs. Goliath': 37, 
+    'Survivor: Edge of Extinction': 38, 
+    'Survivor: Island of the Idols': 39, 
+    'Survivor: Winners at War': 40}, inplace=True) #.infer_objects(copy=False) 
+
+
+
 # Then webscrape new table on idols/ whatever ig?
 # Have found a site with some tables on idol, advantage/disadvantage, immunity stats
 # Determining whether to make add this info to the contestant table, or its own table
 # If it's own table, probably need to find a way to convert the contestant name to the contestant tables index? (foreign key)
 
 
-# Need to change 'Subtitle' value on season table for 1st season to 'Borneo' (remove[c]) - Simple replace or loc
-# It may be better to change the Season on contestant table to int only. So I can match to season table - Something like 'Season' = i+1? i=0 
+
 
 contestant_table
